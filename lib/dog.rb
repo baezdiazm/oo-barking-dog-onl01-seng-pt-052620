34c17a872efa88a44d3dfcd2ef306b2cb2dbc5ac
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-06-16 21:50:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,15 @@
 # Your code goes here!
 class Dog
 
-def name=(dogname)
-@this_dog_name = dogname
-end
+  def name=(dogname)
+    @this_dog_name = dogname
+  end
 
-def name
-@this_dog_name
-end
+  def name
+    @this_dog_name
+  end
 
-def bark
-  puts "woof!"
-end
+  def bark
+    puts "woof!"
+  end
 end
